fb3027ad4964bf0ba709866662c72a7396fc9706
anakin-dabir <anakindabir@gmail.com>
2025-01-20 00:05:28 +0500

    Adding venv

--- projects/vagrant-hosting/automated/scripts/server.sh
+++ projects/vagrant-hosting/automated/scripts/server.sh
@@ -19,6 +19,9 @@ cd $PROJECT_DIR/server-vm && \
 	npm install && \
 	npm install -g pm2
 
+# adding .venv
+wget -P $PROJECT_DIR/server-vm/ http://172.16.0.2/.env
+
 # running the project
 pm2 start index.js -n express-server
 pm2 start ./redis/workers/index.js -n reddis-server
